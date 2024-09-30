e848e2753eb4e0ff38ffa21f95f7a86b78e80033
Sarah <sarah.gemperlein@gmail.com>
2024-09-30 16:25:17 +0200

    basic event handler + lib in ts comfig changes -removing errors.

--- frontend/serviceworker.ts
+++ frontend/serviceworker.ts
@@ -1,6 +1,27 @@
 import { precacheAndRoute } from 'workbox-precaching'
 
-declare var self: ServiceWorkerGlobalScope
+declare let self: ServiceWorkerGlobalScope
 export {}
 
 precacheAndRoute(self.__WB_MANIFEST)
+
+self.addEventListener('install', (e: Event) => {
+  // e.waitUntil(self.skipWaiting())
+  console.log('triggerd activation')
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (e: Event) => {
+  // e.waitUntil(self.clients.claim())
+  console.log('triggerd installation')
+})
+
+self.addEventListener('fetch', (event: Event) => {
+  // event.respondWith(
+  //   caches.match(event.request).then((response) => {
+  //     // Return cached response if available, otherwise fetch from network
+  //     return response || fetch(event.request)
+  //   })
+  // )
+  console.log('triggerd fetch')
+})
